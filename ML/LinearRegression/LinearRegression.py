aa3dff82670958d87cb75bc1f320796ce1dae040
Jeffrey-Adjei <118208940+Jeffrey-Adjei@users.noreply.github.com>
2025-03-28 03:18:29 +0000

    Updated LinearRegressionModel class with gradient_descent and plotting yet to be complete

--- ML/LinearRegression/LinearRegression.py
+++ ML/LinearRegression/LinearRegression.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklear.linear_model import LinearRegression 
+from sklearn.metrics import r2, mean_square_error
+
 
 class LinearRegressionModel:
     def __init__(self, weight, bias, alpha):
@@ -20,4 +22,26 @@ class LinearRegressionModel:
         return x_train, x_test, y_train, y_test
 
     def train_linear_model(x_train, y_train):
-        model = 
+        model = LinearRegression()
+        model.fit(x_train, y_test)
+        return model
+    
+    def model_predictions(model, x_test):
+        y_pred = model.predict(x_test)
+        return y_pred
+
+    def evaluate_regression_model(y_pred):
+        r2_score = r2(y_test, y_pred)
+        mse = mean_square_error(y_test, y_pred)
+        return r2_score, mse
+
+    def gradient_descent_optimisation(m_now, b_now, data, self.alpha):
+        pass
+
+    def plot_regression_line():
+        pass
+
+
+
+if "__name__" == '__main__':
+    main()
