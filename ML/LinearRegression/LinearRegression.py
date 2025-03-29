a5e92fd8f8fc626aac6bafad624cf8a7129815c4
Jeffrey-Adjei <118208940+Jeffrey-Adjei@users.noreply.github.com>
2025-03-29 05:50:45 +0000

    LinearRegression.py adjusted for a single independent variable. This will be performed on a simple dataset (rawdata.csv)

--- ML/LinearRegression/LinearRegression.py
+++ ML/LinearRegression/LinearRegression.py
@@ -1,24 +1,23 @@
-import numpy as np 
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklear.linear_model import LinearRegression 
 from sklearn.metrics import r2, mean_square_error
 
-
 class LinearRegressionModel:
-    def __init__(self, weight, bias, alpha):
-        self.weight = 0
-        self.bias = 0
-        self.alpha = 0.01
+    def __init__(self, weight, bias, alpha, epochs):
+        self.weight = 0.0
+        self.bias = 0.0
+        self.alpha = 0.0001
+        self.epochs = 1500
 
     def load_dataset(self):
-        data = pd.read_csv('NVIDIA Historical Market Data.csv')
-        x = data.iloc[: , :-1]
-        y = data[data.columns[:-1]]
+        data = pd.read_csv('rawdata.csv')
+        x = data['X']
+        y = data['Y']
         return x, y
 
     def split_dataset(x, y):
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42) 
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)          
         return x_train, x_test, y_train, y_test
 
     def train_linear_model(x_train, y_train):
@@ -33,10 +32,27 @@ class LinearRegressionModel:
     def evaluate_regression_model(y_pred):
         r2_score = r2(y_test, y_pred)
         mse = mean_square_error(y_test, y_pred)
-        return r2_score, mse
+        print(f"coefficient of determination: ", {r2_score})
+        print(f"mean square error: ", {mse})
 
-    def gradient_descent_optimisation(m_now, b_now, data, self.alpha):
-        pass
+
+    def gradient_descent_optimisation(m_now, b_now, points, self.alpha):
+        m_gradient = 0 
+        b_gradient = 0 
+
+        n = len(points)
+
+        for i in range(n):
+            x = points.iloc[i].X
+            y = points.iloc[i].Y
+
+            m_gradient += -(2/n) * x * (y - (self.weight * x + self.bias)) 
+            b_gradient += -(2/n) * (y - (self.weight * x + self.bias)) 
+        
+        self.weight = self.weight - m_gradient * self.alpha
+        self.bias = self.bias - b_gradient * self.alpha
+
+        return self.weight, self.bias
 
     def plot_regression_line():
         pass
